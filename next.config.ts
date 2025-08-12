ff51bb1ba44b812f736c56c71fa8f9d71a94965f
Chimaje Agada <94375715+chimaje@users.noreply.github.com>
2025-08-12 18:37:55 +0100

    File creation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
